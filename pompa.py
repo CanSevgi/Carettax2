1981dea0b0e88a354d1d857b24b5ffadad69d9ff
CanSevgi <hcansevgii@gmail.com>
2018-07-13 11:42:17 +0300

    new features

--- pompa.py
+++ pompa.py
@@ -1,32 +1,49 @@
 import RPi.GPIO as gpio
 import time
  
+
 def init():
  gpio.setmode(gpio.BCM)
+ #Motor1
  gpio.setup(17, gpio.OUT)
  gpio.setup(22, gpio.OUT)
+
+ #Motor2
  gpio.setup(23, gpio.OUT)
  gpio.setup(24, gpio.OUT)
  
-def forward(tf):
+def pump():
  init()
+ print ("Motor1 Forward Motor2 Off")
+ #Motor1 (Forward)
  gpio.output(17, True)
  gpio.output(22, False)
- gpio.output(23, True) 
+
+#Motor2 (Off)
+ gpio.output(23, False) 
  gpio.output(24, False)
- time.sleep(tf)
- gpio.cleanup()
  
-def reverse(tf):
+ 
+def depump():
  init()
+ print ("Motor1 Reverse Motor2 Off")
+ #Motor 1 (Reverse)
  gpio.output(17, False)
  gpio.output(22, True)
+
+#Motor2 (off)
  gpio.output(23, False) 
- gpio.output(24, True)
- time.sleep(tf)
- gpio.cleanup()
+ gpio.output(24, False)
+
+def stoppump():
+    init()
+    gpio.cleanup()
+    #Or :
+    #Motor1 (Off)
+    #gpio.output(17, False)
+    #gpio.output(22, False)
+
+    #Motor2 (Off)
+    #gpio.output(23, False) 
+    #gpio.output(24, False)
  
-print ("forward")
-forward(4)
-print ("backward")
-reverse(2)
