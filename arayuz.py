5d4d162e2c6b85cfc8da9ea3e1c32355237646a8
CanSevgi <hcansevgii@gmail.com>
2018-07-13 11:53:48 +0300

    bug fix and cleaning
    Gereksiz satırları temizleyip görünmeyen butonu ekledim, butonların sırasını değiştirdim kullanım kolaylığı için

--- arayuz.py
+++ arayuz.py
@@ -10,31 +10,17 @@ bottomFrame.pack(side=BOTTOM)
 
 
 
-button1 = Button(topFrame, text="Su Al",bg="Green", command=pompa.pump())
-button2 = Button(topFrame, text="Su Ver",bg="red",command=pompa.depump())
-button3 = Button(topFrame, text="Stop Pumping", bg = "yellow", command = pompa.stoppump())
+button1 = Button(topFrame, text="Su Al",bg="Green", command=pompa.pump)
+button2 = Button(topFrame, text="Stop Pumping",bg="yellow",command=pompa.stoppump)
+button3 = Button(topFrame, text="Su Ver", bg = "red", command = pompa.depump)
 
 button1.pack()
 button2.pack()
+button3.pack()
 
 
 status = Label(root, text="Nothing", bd=1,relief=SUNKEN,anchor = W)
 status.pack(side=BOTTOM,fill=X)
-# def leftClick(event):
-#     print("Left")
 
-# def midClick(event):
-#     print("mid")
-
-# def rightClick(event):
-#     print("right")
-
-
-
-# frame = Frame(root, width = 300, height = 250)
-# frame.bind("<Button-1>",leftClick)
-# frame.bind("<Button-2>",midClick)
-# frame.bind("<Button-3>",rightClick)
-# frame.pack()
 
 root.mainloop()
