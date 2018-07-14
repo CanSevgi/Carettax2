d72f5827d5a6b5dc0ee03d43898024cb506c73ae
Onurcan <onur-koken@hotmail.com>
2018-07-14 16:46:02 +0300

    PWM motor kontrol
    Bu kod örnek bir 2 motorlu motor kontrol kodudur,  denenmesi gerekiyor, özellikle turn fonksiyonları denenmeli ki pwm çalışıyor mu anlaşılmalı.

---
+++ PWM motor controls.py
@@ -0,0 +1,99 @@
+#This code tests L298N H-Bridge driver module with Raspberry Pi GPIO PWM connections
+
+
+#gpiozero kullanımı için pip yüklenmesi gerekebilir ancak raspberry'de çalışmalı
+from gpiozero import PWMOutputDevice
+from time import sleep
+
+#Defining Motor Drive GPIO Pins
+#Motor 1
+PWM_Forward_left_pin = 26
+PWM_Reverse_left_pin = 19
+#Motor 2
+PWM_Forward_right_pin = 13
+PWM_Reverse_right_pin = 6
+
+#Initialise objects for H-Bridge PWM pins
+#Set initial duty cycle to 0 and frequency to 1000
+forwardLeft = PWMOutputDevice(PWM_Forward_left_pin, True, 0, 1000)
+reverseLeft = PWMOutputDevice(PWM_Reverse_left_pin, True, 0, 1000)
+
+forwardRight = PWMOutputDevice(PWM_Forward_right_pin, True, 0, 1000)
+reverseRight = PWMOutputDevice(PWM_Reverse_right_pin, True, 0, 1000)
+
+def allStop():
+    forwardLeft.value = 0
+    reverseLeft.value = 0
+    forwardRight.value = 0
+    reverseRight.value = 0
+
+def forwardDrive():
+    forwardLeft.value = 1.0
+    reverseLeft.value = 0
+    forwardRight.value = 1.0
+    reverseRight.value = 0
+
+def reverseDrive():
+    forwardLeft.value = 0
+    reverseLeft.value = 1.0
+    forwardRight.value = 0
+    reverseRight.value = 1.0
+
+def spinLeft():
+    forwardLeft.value = 0
+    reverseLeft.value = 1.0
+    forwardRight.value = 1.0
+    reverseRight.value = 0
+
+def SpinRight():
+    forwardLeft.value = 1.0
+    reverseLeft.value = 0
+    forwardRight.value = 0
+    reverseRight.value = 1.0
+
+def forwardTurnLeft():
+    forwardLeft.value = 0.2
+    reverseLeft.value = 0
+    forwardRight.value = 0.8
+    reverseRight.value = 0
+
+def forwardTurnRight():
+    forwardLeft.value = 0.8
+    reverseLeft.value = 0
+    forwardRight.value = 0.2
+    reverseRight.value = 0
+
+def reverseTurnLeft():
+    forwardLeft.value = 0
+    reverseLeft.value = 0.2
+    forwardRight.value = 0
+    reverseRight.value = 0.8
+
+def reverseTurnRight():
+    forwardLeft.value = 0
+    reverseLeft.value = 0.8
+    forwardRight.value = 0
+    reverseRight.value = 0.2
+
+def main():
+    allStop()
+    forwardDrive()
+    sleep(5)
+    reverseDrive()
+    sleep(5)
+    spinLeft()
+    sleep(5)
+    forwardTurnLeft()
+    sleep(5)
+    forwardTurnRight()
+    sleep(5)
+    reverseTurnLeft()
+    sleep(5)
+    reverseTurnRight()
+    sleep(5)
+    allStop()
+
+#bu kısmı anlayamadım ??? neden if var ?
+if __name__ == "__main__":
+    """ This is executed when run from the command line """
+    main()
