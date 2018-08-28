313dd545db69f8d949b3b3adb3935204cc018db7
Onurcan <onur-koken@hotmail.com>
2018-08-28 17:29:06 +0300

    servo pigpio
    servo_acı.py nin pigpio hali

---
+++ motor/servo_acı_pigpio.py
@@ -0,0 +1,27 @@
+import pigpio
+import time
+#başlatmadan önce sudo pigpiod komutunu çalıştırın
+servoPIN = 18
+
+pi = pigpio.pi() #GPIO.setmode(GPIO.BCM)
+
+pi.set_mode(servoPIN, pigpio.OUTPUT) #GPIO.setup(servoPIN, GPIO.OUT)
+
+#GPIO 18 for PWM with 50Hz
+p = pi.set_PWM_frequency(servoPIN,100) #p = GPIO.PWM(servoPIN, 100)
+
+def servo(acı):
+    cyclle = (int(acı) * 255)/180 #calculation of the cycle
+    #p.start(0)
+    #p.ChangeDutyCycle(cyclle)
+    pi.set_PWM_dutycycle(servoPIN,cyclle)
+    time.sleep(0.5)
+    #p.ChangeDutyCycle(0)
+
+try:
+    while True:
+        acı = input("enter acı")
+        servo(acı)
+        
+except KeyboardInterrupt:
+    GPIO.cleanup()
